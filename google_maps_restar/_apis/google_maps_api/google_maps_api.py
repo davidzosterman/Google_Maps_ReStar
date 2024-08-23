30152fc0ecc980d7e84ff3c96da1a0765d30ca71
David Z Osterman <d.z.osterman@gmail.com>
2024-08-23 16:20:00 -0400

    Add files via upload

---
+++ google_maps_restar/_apis/google_maps_api/google_maps_api.py
@@ -0,0 +1,25 @@
+import requests
+
+def __call_api(endpoint: str):
+    response = requests.get(f'https://maps.googleapis.com/maps/api/place{endpoint}')
+    print(f'Response status code: {response.status_code}')
+    return response.json()
+
+def find_place_from_text(API_KEY: str, restaurant_name: str):
+    endpoint = (f'/findplacefromtext/json'
+       f'?fields=formatted_address%2Cname%2Crating%2Cplace_id'
+       f'&input={restaurant_name}'
+       f'&inputtype=textquery'
+       f'&key={API_KEY}'
+      )
+    response = __call_api(endpoint)
+    return response
+
+def place_details(API_KEY: str, place_id: str):
+    endpoint = (f'/details/json'
+       f'?fields=reviews'
+       f'&place_id={place_id}'
+       f'&key={API_KEY}'
+      )
+    response = __call_api(endpoint)
+    return response
